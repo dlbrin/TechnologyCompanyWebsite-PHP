42c0ae8970ec6c19fb2f3a5711338daaf26ed649
DlbrinAzad <69488118+dlbrin@users.noreply.github.com>
2022-03-29 21:45:33 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+</head>
+<body>
+	<center>
+	    <footer>
+	    	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="white" fill-opacity="1" d="M0,192L48,181.3C96,171,192,149,288,149.3C384,149,480,171,576,197.3C672,224,768,256,864,256C960,256,1056,224,1152,176C1248,128,1344,64,1392,32L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+	    	<div class="contact-us" id="contact">
+	    		<div class="contact-details" data-aos="zoom-in" data-aos-duration="1500">
+	    			<form method="POST" action="modal/contact.inc.php" enctype="multipart/form-data">
+	    				<h1>CONTACT US</h1>
+	    				<input type="text" name="name" placeholder="Enter Your Name">
+	    				<br>
+	    				<input type="text" name="email" placeholder="Enter Your Email">
+	    				<br>
+	    				<input type="text" name="subject" placeholder="Enter Your Subject">
+	    				<br>
+	    				<textarea name="message" placeholder="Message"></textarea>
+	    				<br>
+	    				<button type="submit" name="submit">SEND</button>
+	    			</form>
+	    		</div>
+	    		<div class="address-details" data-aos="zoom-in" data-aos-duration="1500">
+	    			<h1>ADDRESS</h1>
+	    			<h2><i class="fas fa-phone"></i> &nbsp <a href="callto:+9647504836165">
+                    +9647504836165</a></h2>
+                    <h2><i class="far fa-envelope"></i> &nbsp <a href="mailto:info@easy-tech.co">
+                    info@easy-tech.co</a></h2>
+                    <h2><i class="far fa-building"></i> &nbsp <a href="#">
+                    KRO_Nisry Building_Floor-2/Apartment-6</a></h2>
+                    <h2><i class="fab fa-facebook"></i> &nbsp <a href="https://www.facebook.com/2019EasyTech/">
+                    EASY TECH CO.</a></h2>
+                    <h2><i class="fab fa-instagram"></i> &nbsp <a href="https://www.instagram.com/easy_tech_company/?igshid=1bn5if5k61wsn">
+                    easy_tech_company</a></h2>
+	    		</div>
+	    	</div>
+	    	<br>
+		    <div class="footer-details">
+		        <div class="social-media">
+			        <h3>©2021 Easy Technology. All rights reserved.<br> Design and Developed by <a href="https://www.facebook.com/dlbren.akre/">Dlbrin Azad</a>.</h3>
+		        </div>
+		    </div>
+	    </footer>
+    </center>
+    <script>
+    AOS.init();
+    </script>
+</body>
+</html>
---
+++ includes/nav.php
@@ -0,0 +1,31 @@
+<?php include "config.php" ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+</head>
+<body>
+	<nav>
+		<div class="logo">
+			<a href="index.php"><img src="assets/img/EasyTechLogo.png"></a>
+		</div>
+		<div class="openMenu"><i class="fa fa-bars"></i></div>
+		<ul class="mainMenu">
+			<li><a href="index.php">Home</a></li>
+            <li><a href="news.php">News</a></li>
+            <li><a href="#service">Our Services</a></li>
+            <li><a href="courses.php">Courses</a></li>
+            <li><a href="#projects">Last Projects</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <div class="closeMenu"><i class="fa fa-times"></i></div>
+        </ul>
+	</nav>
+
+	<script src="assets/js/app.js"></script>
+</body>
+</html>
